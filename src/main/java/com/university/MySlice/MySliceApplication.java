1a555174333ac916f400317a95c494d51e5b9140
Yash Patel <62371168+yashpatel4900@users.noreply.github.com>
2023-06-30 11:00:02 -0400

    New Project

---
+++ src/main/java/com/university/MySlice/MySliceApplication.java
@@ -0,0 +1,14 @@
+package com.university.MySlice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySliceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MySliceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/university/MySlice/MySliceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.MySlice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySliceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
